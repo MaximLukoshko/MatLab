5f89fcf769537afd59cd11f1c9898fc4781bb917
MaximLukoshko <www.maxim.by97@gmail.com>
2015-11-29 14:36:18 +0300

    Written the initial condition of temperature

--- Partial_differential_equations/PartialDifferentialEquations.m
+++ Partial_differential_equations/PartialDifferentialEquations.m
@@ -7,3 +7,22 @@ WIDTH = 20;
 HEIGHT = 12; 
 HeightLeftZone = 10; 
 HeightRightZone = 7;
+
+h = 1;
+i_max = floor(WIDTH / h);
+j_max = floor(HEIGHT / h);
+j_max_left = floor(HeightLeftZone / h) ;
+j_min_right = j_max - ceil(HeightRightZone / h);
+
+Temperatures = zeros(j_max, i_max);
+
+for k=j_max - j_max_left + 1:j_max
+   Temperatures(k, 1) = TLeft; 
+end
+
+for k=1:j_max - j_min_right;
+   Temperatures(k, i_max) = TRight; 
+end
+
+
+disp(Temperatures);
