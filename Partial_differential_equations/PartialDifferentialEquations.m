c95c1d9904e6dad28531811153506fb9d7ffa4d7
MaximLukoshko <www.maxim.by97@gmail.com>
2015-11-29 16:53:35 +0300

    Built some surface, written a shell of main cicle

--- Partial_differential_equations/PartialDifferentialEquations.m
+++ Partial_differential_equations/PartialDifferentialEquations.m
@@ -8,21 +8,33 @@ HEIGHT = 12;
 HeightLeftZone = 10; 
 HeightRightZone = 7;
 
+MAX_ITER = 1000;
+eps = 0.001;
+
 h = 1;
 i_max = floor(WIDTH / h);
 j_max = floor(HEIGHT / h);
 j_max_left = floor(HeightLeftZone / h) ;
 j_min_right = j_max - ceil(HeightRightZone / h);
 
-Temperatures = zeros(j_max, i_max);
+Temperatures = repmat(TRight, j_max, i_max);
 
 for k=j_max - j_max_left + 1:j_max
    Temperatures(k, 1) = TLeft; 
 end
 
-for k=1:j_max - j_min_right;
-   Temperatures(k, i_max) = TRight; 
-end
-
+% for k=1:j_max - j_min_right;
+%    Temperatures(k, i_max) = TRight; 
+% end
 
 disp(Temperatures);
+
+surf(Temperatures);
+
+deltaMax = 1 + eps;
+num_iter = 0;
+
+while num_iter < MAX_ITER && deltaMax > eps
+    num_iter = num_iter + 1;
+    
+end
