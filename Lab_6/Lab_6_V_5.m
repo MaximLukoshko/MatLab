bb49c08364834c5f8c743e72fa18d5a0882c777c
MaximLukoshko <www.maxim.by97@gmail.com>
2015-12-02 00:15:31 +0300

    Fixed generating unused realisations

--- Lab_6/Lab_6_V_5.m
+++ Lab_6/Lab_6_V_5.m
@@ -5,13 +5,10 @@ Lambda = 0.5;
 OmegaLeft = 0;
 OmegaRight = 5;
 M=1000;
-Intervals=zeros(1,1);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-for i=1:M
-    Intervals = model(Lambda, OmegaLeft, OmegaRight, M);
-end
-disp(Intervals);
+
+Intervals = model(Lambda, OmegaLeft, OmegaRight, M);
 
 k=1:1:M;
 Events_Density_pract =  Intervals ./ ((OmegaRight-OmegaLeft));
@@ -32,14 +29,10 @@ hold off
 
 k=0:1:10;
 
-
 PDF_theor = poisspdf(k, Lambda * (OmegaRight-OmegaLeft));
-
-step = (OmegaRight-OmegaLeft);
 
 PDF_pract = hist(Intervals, k);
 PDF_pract = PDF_pract ./ length(Intervals);
-
 
 figure
 hold on
@@ -48,6 +41,3 @@ plot(k , PDF_theor, 'g', 'LineWidth', 5)
 title('PDF');
 hold off
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-disp('Hi');
